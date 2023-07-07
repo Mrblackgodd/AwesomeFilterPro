0b9d4436b01b8bb960d996ad3b17a7ba09b465e5
Mrblackgodd <136906179+Mrblackgodd@users.noreply.github.com>
2023-07-07 08:36:13 +0530

    Update info.py

--- info.py
+++ info.py
@@ -14,7 +14,7 @@ def is_enabled(value, default):
 SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(environ.get('API_ID', '25377875'))
 API_HASH = environ.get('API_HASH', 'cf80e342be48570ca2e4c9d2c7695413')
-BOT_TOKEN = environ.get('BOT_TOKEN', '5867415844:AAF6OWScDJMeGoZfiBBQbJxHK7XmWF_xp30')
+BOT_TOKEN = environ.get('BOT_TOKEN', '6354598840:AAFQHMSN0A2VJyo9C-ipXgaCZ1N2Um-WvSY')
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', False))
@@ -31,7 +31,7 @@ AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_chan
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Mrblackgod:<nehalsingh969797>@cluster0.oodoxwm.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Mrblackgod:nehalsingh969797@cluster0.gejltut.mongodb.net/?retryWrites=true&w=majority")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Rajappan")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
